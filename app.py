f5653d6540f3f6af00fcd1aca0b7ec4f24ad08e8
manvisakhi-prog <manvisakhi@gmail.com>
2026-04-28 21:49:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,18 +97,18 @@ if st.button("RUN ANALYSIS"):
     st.divider()
     st.subheader(f"Probability of Approval: {prob:.1%}")
     
-   if prob > 0.70:
-        st.success("✅ **High Confidence Approval**")
-        st.info(f"💰 **Strategy:** Route to **Lender B**. Applicant qualifies for our premium partner. **Estimated Payout: $350**")
-        
-    elif prob >= 0.50:
-        st.success("✅ **Standard Approval**")
-        st.info(f"💰 **Strategy:** Route to **Lender A**. Solid applicant, but may not meet Lender B's strict tier. **Estimated Payout: $250**")
-        
-    elif prob > 0.35:
-        st.warning("⚠️ **Marginal Approval**")
-        st.info(f"💰 **Strategy:** Route to **Lender C**. Higher likelihood of closing with this flexible partner. **Estimated Payout: $150**")
-        
-    else:
-        st.error("🚫 **Application Denied**")
-        st.write("The risk profile exceeds the tolerance of our current lending partners (A, B, or C).")
+       if prob > 0.70:
+            st.success("✅ **High Confidence Approval**")
+            st.info(f"💰 **Strategy:** Route to **Lender B**. Applicant qualifies for our premium partner. **Estimated Payout: $350**")
+            
+        elif prob >= 0.50:
+            st.success("✅ **Standard Approval**")
+            st.info(f"💰 **Strategy:** Route to **Lender A**. Solid applicant, but may not meet Lender B's strict tier. **Estimated Payout: $250**")
+            
+        elif prob > 0.35:
+            st.warning("⚠️ **Marginal Approval**")
+            st.info(f"💰 **Strategy:** Route to **Lender C**. Higher likelihood of closing with this flexible partner. **Estimated Payout: $150**")
+            
+        else:
+            st.error("🚫 **Application Denied**")
+            st.write("The risk profile exceeds the tolerance of our current lending partners (A, B, or C).")
